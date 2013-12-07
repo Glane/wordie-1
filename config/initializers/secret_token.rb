aa332f3df10ff3e19e2e3e0a8023740c8e4039e2
casecode <casey.r.white@gmail.com>
2013-12-07 17:56:19 -0600

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Wordie::Application.config.secret_key_base = '7283aaf5de3041a03246c19e18e7731d310a80dc0dbd604fbb02ddf0b760c9759d9d8543ef1d6a847da352063d2087667dd93c7b216f50990479d58b3dbb4f87'
