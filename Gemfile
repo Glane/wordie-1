e404adb529d212aff7313b6da04ff88bc9831923
casecode <casey.r.white@gmail.com>
2013-12-07 22:39:20 -0600

    add twitter auth keys and config, and add class method in user class to call client in instance method

--- Gemfile
+++ Gemfile
@@ -14,6 +14,8 @@ gem 'foundation-rails'
 gem 'pg'
 gem 'rolify'
 gem 'simple_form'
+gem 'twitter'
+gem 'unirest'
 group :development do
   gem 'better_errors'
   gem 'binding_of_caller', :platforms=>[:mri_19, :mri_20, :rbx]
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,20 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  # Class Method to call client in instance methods
+  #
+  # Example:
+	# 		def instance_method
+	# 			User.client_call.user(@twitter_screenname).some_twitter_method
+	# 		end
+	def self.client_call
+		Twitter::REST::Client.new do |config|
+			config.consumer_key        = ENV['CONSUMER_KEY']
+			config.consumer_secret     = ENV['CONSUMER_SECRET']
+			config.access_token         = ENV['ACCESS_TOKEN']
+			config.access_token_secret  = ENV['ACCESS_TOKEN_SECRET']
+		end
+	end
+
 end
